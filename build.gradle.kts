e514dab556ec6acc9f0ec52e2736722f0427e731
Dertfin3051 <dertfin@gmail.com>
2024-10-03 20:20:44 +0300

    Remove this fckin' junit from dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,10 +15,4 @@ tasks.jar {
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-}
-
-tasks.test {
-    useJUnitPlatform()
 }
