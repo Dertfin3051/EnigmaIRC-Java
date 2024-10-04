5f55bb08842e734ba7d8c34193b10ebffe00fec9
Dertfin3051 <dertfin@gmail.com>
2024-10-04 18:20:45 +0300

    Handle new incoming messages

--- client/src/main/java/ru/dfhub/eirc/DataParser.java
+++ client/src/main/java/ru/dfhub/eirc/DataParser.java
@@ -2,8 +2,14 @@ package ru.dfhub.eirc;
 
 import org.json.JSONObject;
 
+/**
+ * Class for working with data and processing it
+ */
 public class DataParser {
 
+    /**
+     * Types of incoming and outgoing messages
+     */
     public enum MessageType {
         USER_MESSAGE, USER_SESSION
     }
@@ -21,7 +27,20 @@ public class DataParser {
         Gui.showNewMessage(data, Gui.MessageType.USER_MESSAGE);
     }
 
+    /**
+     * Processing an incoming user message
+     * @param data Data's "content" object
+     */
     private static void handleUserMessage(JSONObject data) {
-        // Format message to string, check if it's self message and show it correctly
+        String sender = data.getString("user");
+        String message = data.getString("message"); // In ftr, decrypt and handle decryption errors here
+
+        String formattedMessage = "%s -> %s".formatted(sender, message); // In ftr, handle timestamps here
+
+        if (sender.equals(Main.getConfig().getString("username"))) {
+            Gui.showNewMessage(formattedMessage, Gui.MessageType.SELF_USER_MESSAGE);
+        } else {
+            Gui.showNewMessage(formattedMessage, Gui.MessageType.USER_MESSAGE);
+        }
     }
 }
