ad59cfb1e92f26f7bbdb7b972371fcbacdb3f64f
Dertfin3051 <dertfin@gmail.com>
2024-10-04 17:55:10 +0300

    Add ServerConnection thread

--- client/src/main/java/ru/dfhub/eirc/Main.java
+++ client/src/main/java/ru/dfhub/eirc/Main.java
@@ -1,8 +1,23 @@
 package ru.dfhub.eirc;
 
 public class Main {
+
+    private static ServerConnection serverConnection;
+
     public static void main(String[] args) {
         Gui.init();
         Gui.showWelcomeMessage();
+
+        try {
+            serverConnection = new ServerConnection("127.0.0.1", 6667);
+        } catch (Exception e)
+        {
+            Gui.breakInput();
+            Gui.showNewMessage("Failed connect to the server!", Gui.MessageType.SYSTEM_ERROR);
+        }
+    }
+
+    public ServerConnection getServerConnection() {
+        return serverConnection;
     }
 }
---
+++ client/src/main/java/ru/dfhub/eirc/ServerConnection.java
@@ -0,0 +1,39 @@
+package ru.dfhub.eirc;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ * Class for working with the server. Receiving and sending messages is done here.
+ */
+public class ServerConnection extends Thread {
+
+    private Socket server;
+    private BufferedReader in;
+    private PrintWriter out;
+
+    public ServerConnection(String serverAddress, int serverPort) throws Exception {
+        server = new Socket(serverAddress, serverPort);
+        in = new BufferedReader(new InputStreamReader(server.getInputStream()));
+        out = new PrintWriter(server.getOutputStream(), true);
+
+        this.start();
+    }
+
+    @Override
+    public void run() {
+        while (server.isConnected()) {
+            // Read and parse data
+        }
+    }
+
+    /**
+     * Send information to the server
+     * @param data Data
+     */
+    public void sendToServer(String data) {
+        out.write(data);
+    }
+}
