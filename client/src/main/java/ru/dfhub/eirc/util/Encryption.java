2975c89d2e2ef42b529915819d39f383047958a8
Dertfin3051 <dertfin@gmail.com>
2024-10-06 14:21:51 +0300

    Add encrypt&decrypt methods
    (cherry picked from commit d93dafc87ff2d399b20df7443e27b93a766b49ca)

--- client/src/main/java/ru/dfhub/eirc/util/Encryption.java
+++ client/src/main/java/ru/dfhub/eirc/util/Encryption.java
@@ -8,6 +8,7 @@ import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.security.InvalidKeyException;
@@ -86,5 +87,29 @@ public class Encryption {
      */
     public static void generateNewKeyFile() throws IOException {
         Files.writeString(Paths.get("newkey.txt"), "New encryption key: %s".formatted(generateNewKey()));
+    }
+
+    /**
+     * Encrypt text
+     * @param text Text
+     * @return Encrypted text (base64)
+     * @throws Exception Wrong encryption key
+     */
+    public static String encrypt(String text) throws Exception {
+        byte[] encryptedBytes = encryptCipher.doFinal(text.getBytes(StandardCharsets.UTF_8));
+        return Base64.getEncoder().encodeToString(encryptedBytes);
+    }
+
+    /**
+     * Decrypt text
+     * @param text Encrypted text (base64)
+     * @return Decrypted text
+     * @throws Exception Wrong encryption key
+     */
+    public static String decrypt(String text) throws Exception {
+        byte[] encryptedBytes = Base64.getDecoder().decode(text);
+        byte[] decryptedBytes = decryptCipher.doFinal(encryptedBytes);
+
+        return new String(decryptedBytes);
     }
 }
