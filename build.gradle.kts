949f602258be77c932e5bef4067fe8eddcb66172
Dertfin3051 <dertfin@gmail.com>
2024-10-07 22:04:26 +0300

    Change version to 2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "ru.dfhub.eirc"
-version = "1.0-SNAPSHOT"
+version = "2.0"
 
 allprojects {
     repositories {
--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -1,5 +1,5 @@
 group = "ru.dfhub.eirc"
-version = "1.0-SNAPSHOT"
+version = "2.0"
 
 application {
     mainClass = "ru.dfhub.eirc.Main"
