c737d13efa621eec59198e1150f3118259037e16
Dertfin3051 <dertfin@gmail.com>
2024-10-24 17:33:14 +0300

    Remove "Colored" dependency Useless

--- server/src/main/java/ru/dfhub/eirc/Main.java
+++ server/src/main/java/ru/dfhub/eirc/Main.java
@@ -1,7 +1,5 @@
 package ru.dfhub.eirc;
 
-import io.github.Dertfin3051.Color;
-import io.github.Dertfin3051.Colored;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.json.JSONObject;
