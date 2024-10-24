fac074e7bbcc26a682c323f983d128891a8b9f79
Dertfin3051 <dertfin@gmail.com>
2024-10-24 16:35:44 +0300

    Add log4j implementation

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -7,4 +7,6 @@ application {
 
 dependencies {
     implementation("com.github.Dertfin3051:Colored:1.3")
+    implementation("org.apache.logging.log4j:log4j-core:2.24.1")
+    implementation("org.apache.logging.log4j:log4j-api:2.24.1")
 }
